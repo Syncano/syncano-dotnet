be3ff687afd5ebcf3748708b66142a73eb9819fe
maciej.szczepanski <maciej.szczepanski@outlook.com>
2014-07-09 02:03:44 +0200

    Initial projects

---
+++ src/Syncano.Net.Tests/SyncanoRestClientTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Should;
+using Xunit;
+
+namespace Syncano.Net.Tests
+{
+    public class SyncanoRestClientTests
+    {
+        [Fact]
+        public async Task StartSession()
+        {
+            var response = await new SyncanoRestClient().StartSession("icy-brook-267066", "2347b3ee5d7a9b13622e2812e34aaa7351dc7cbb");
+         
+            response.Result.ShouldEqual("OK");
+            response.SessionId.ShouldNotBeNull();
+        }
+    }
+}
---
+++ src/Syncano.Net/SyncanoRestClient.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Syncano.Net
+{
+    public class SyncanoRestClient
+    {
+        private HttpClient _client;
+
+        public SyncanoRestClient()
+        {
+            _client = new HttpClient(new HttpClientHandler() {AutomaticDecompression = DecompressionMethods.Deflate | DecompressionMethods.GZip, UseCookies = true});
+        }
+
+        public async Task<Response> StartSession(string instanceName, string userApiKey)
+        {
+            var response = await _client.GetStringAsync(string.Format("https://{0}.syncano.com/api/apikey.start_session?api_key={1}", instanceName, userApiKey));
+
+            return JsonConvert.DeserializeObject<Response>(response);
+        }
+    }
+
+
+    public class Response
+    {
+        [JsonProperty("result")]
+        public string Result { get; set; }
+
+        [JsonProperty("session_id")]
+        public string SessionId { get; set; }
+    }
+}
