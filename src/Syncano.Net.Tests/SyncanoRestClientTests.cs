76373982236f8572c875b10bb455c4dfefa78ef0
maciej.szczepanski <maciej.szczepanski@outlook.com>
2014-07-09 23:26:56 +0200

    Added project.get + some refactoring

--- src/Syncano.Net.Tests/SyncanoRestClientTests.cs
+++ src/Syncano.Net.Tests/SyncanoRestClientTests.cs
@@ -1,20 +1,56 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Linq;
 using System.Threading.Tasks;
 using Should;
 using Xunit;
 
 namespace Syncano.Net.Tests
 {
-    public class SyncanoRestClientTests
+    public static class TestData
     {
-        [Fact]
-        public async Task StartSession()
+        public const string InstanceName = "icy-brook-267066";
+
+        public const string BackendAdminApiKey = "f020f3a62b2ea236100a732adcf60cb98683e2e5";
+        
+    }
+
+    public class SyncanoRestClientTests  : IDisposable
+    {
+        private SyncanoRestClient _client;
+        
+
+        public SyncanoRestClientTests()
         {
-            var response = await new SyncanoRestClient().StartSession("icy-brook-267066", "2347b3ee5d7a9b13622e2812e34aaa7351dc7cbb");
+            _client = new SyncanoRestClient(TestData.InstanceName, TestData.BackendAdminApiKey);
+        }
+
+        [Fact]
+        public async Task StartSession_WhenValidInstanceAndKey_CreatesSessionId()
+        {
+            //when 
+            var response = await _client.StartSession();
          
+            //then
             response.Result.ShouldEqual("OK");
             response.SessionId.ShouldNotBeNull();
+        }
+
+        [Fact]
+        public async Task GetProjects()
+        {
+
+            //when
+            var response = await _client.GetProjects();
+
+            //then
+            response.ShouldNotBeEmpty();
+            response.Any(p => p.Name  == "Default").ShouldBeTrue();
+        }
+
+        public void Dispose()
+        {
+            
         }
     }
 }
--- src/Syncano.Net/SyncanoRestClient.cs
+++ src/Syncano.Net/SyncanoRestClient.cs
@@ -1,31 +1,82 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace Syncano.Net
 {
     public class SyncanoRestClient
     {
+        private readonly string _instanceName;
+        private readonly string _apiKey;
         private HttpClient _client;
+        private string _baseUrl;
 
-        public SyncanoRestClient()
+        public SyncanoRestClient(string instanceName, string apiKey)
         {
+            _instanceName = instanceName;
+            _apiKey = apiKey;
+            _baseUrl = string.Format("https://{0}.syncano.com/api/", _instanceName);
             _client = new HttpClient(new HttpClientHandler() {AutomaticDecompression = DecompressionMethods.Deflate | DecompressionMethods.GZip, UseCookies = true});
         }
 
-        public async Task<Response> StartSession(string instanceName, string userApiKey)
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="userApiKey"></param>
+        /// <returns></returns>
+        public async Task<Response> StartSession()
         {
-            var response = await _client.GetStringAsync(string.Format("https://{0}.syncano.com/api/apikey.start_session?api_key={1}", instanceName, userApiKey));
+            var response = await _client.GetStringAsync(CreateGetUri("apikey.start_session"));
 
             return JsonConvert.DeserializeObject<Response>(response);
         }
+
+        private string CreateGetUri(string methodName, ExpandoObject query = null)
+        {
+            var sb = new StringBuilder(_baseUrl);
+            sb.Append(methodName);
+            sb.Append("?api_key=");
+            sb.Append(_apiKey);
+
+            if (query != null)
+            {
+                foreach (var each in query)
+                {
+                    sb.AppendFormat("&{0}={1}", each.Key, Uri.EscapeDataString(each.Value.ToString()));
+                }
+            }
+
+            return sb.ToString();
+        }
+
+        public async Task<List<Project>> GetProjects()
+        {
+            var response = await _client.GetStringAsync(CreateGetUri("project.get"));
+
+            var projects = JObject.Parse(response).SelectToken("project");
+            
+            return projects.ToObject<List<Project>>();
+
+        }
     }
 
+
+    public class Project
+    {
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+    }
 
     public class Response
     {
