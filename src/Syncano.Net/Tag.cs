02b4cda77b424d52aa0c92215ae5549f272ea031
Albert Wolant <albertwolant@gmail.com>
2014-07-10 21:17:23 +0200

    Added: Collection, Tag, CollectionStatus

---
+++ src/Syncano.Net/Tag.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+
+namespace Syncano.Net
+{
+    public class Tag
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("weight")]
+        public decimal Weight { get; set; }
+    }
+}
